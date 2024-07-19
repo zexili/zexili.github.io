08f7c77dfde5711c1f4f9a49d0c3117e911e5dd0
leeyazhou <bytesgo@163.com>
2024-07-19 14:50:13 +0800

    init

---
+++ docs/.vitepress/config.mjs
@@ -0,0 +1,31 @@
+import { defineConfig } from 'vitepress'
+
+// https://vitepress.dev/reference/site-config
+export default defineConfig({
+  title: "zexili",
+  description: "hi there, I'm zexi.li",
+  themeConfig: {
+    // https://vitepress.dev/reference/default-theme-config
+    nav: [
+      { text: '首页', link: '/' },
+      { text: '示例', link: '/markdown-examples' }
+    ],
+
+    sidebar: [
+      {
+        text: '示例',
+        items: [
+          { text: 'Markdown Examples', link: '/markdown-examples' },
+          { text: 'Runtime API Examples', link: '/api-examples' }
+        ]
+      }
+    ],
+
+    socialLinks: [
+      { icon: 'github', link: 'https://github.com/zexili' }
+    ],
+    sitemap: {
+      hostname: 'https://example.com'
+    }
+  }
+})
