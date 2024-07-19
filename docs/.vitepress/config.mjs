95a6f93b9c9a975cabf5dc11d7f9020aa9f5fb7d
leeyazhou <bytesgo@163.com>
2024-07-19 15:27:07 +0800

    init

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'vitepress'
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
-  title: "zexili",
+  title: "ZX",
   description: "hi there, I'm zexi.li",
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
